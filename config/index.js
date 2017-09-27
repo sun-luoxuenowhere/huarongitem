54280900c7f26a3667aecad502404c34308f4c3b
lizhwb <lizhwb@yonyou.com>
2017-09-27 16:52:53 +0800

    地址变更

--- config/index.js
+++ config/index.js
@@ -30,9 +30,9 @@ module.exports = {
     proxyTable: {
     	'/service/': { 
     		//target: 'http://10.4.120.29:80', //zyl(cc)
-    		//target: 'http://10.4.120.26:8090', //lzw
+    		target: 'http://10.4.120.26:8090', //lzw
     		//target: 'http://125.35.5.243:6007', //服务器
-      	target: 'http://10.15.0.216:6007', //服务器
+      	//target: 'http://10.15.0.216:6007', //服务器
         changeOrigin:true 
     	} 
     },
